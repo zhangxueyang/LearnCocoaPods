23370db255b76e3612af2278ee290adaffcbe6ed
张学阳 <>
2018-04-11 22:43:25 +0800

    添加label分类

---
+++ Classes/UILabel+Extension.h
@@ -0,0 +1,13 @@
+//
+//  UILabel+Extension.h
+//  HMObjcSugar
+//
+//  Created by 张学阳 on 2018/4/11.
+//  Copyright © 2018年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (Extension)
+
+@end
---
+++ Classes/UILabel+Extension.m
@@ -0,0 +1,13 @@
+//
+//  UILabel+Extension.m
+//  HMObjcSugar
+//
+//  Created by 张学阳 on 2018/4/11.
+//  Copyright © 2018年 itcast. All rights reserved.
+//
+
+#import "UILabel+Extension.h"
+
+@implementation UILabel (Extension)
+
+@end
